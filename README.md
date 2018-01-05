5433c7d142be777b082d316dc9d3d97b5cced02a
3axap4eHko <3axap4eHko@gmail.com>
2018-01-04 19:39:47 -0500

    :bowtie:

---
+++ README.md
@@ -0,0 +1,46 @@
+# React Native <img src="https://khan.github.io/KaTeX/katex-logo.svg" width="130" alt="KaTeX">
+
+## Usage
+```javascript
+import { StyleSheet } from 'react-native';
+import Katex from 'react-native-katex';
+
+const styles = StyleSheet.create({
+  katex: {
+    flex: 1,
+  }
+});
+
+const inlineStyle =`
+html, body {
+  display: flex;
+  background-color: #fafafa;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+.katex {
+  font-size: 4em;
+  margin: 0;
+  display: flex;
+}
+`;
+
+<Katex
+  expression="c=\pm\sqrt{a^2 + b^2}"
+  style={styles.katex}
+  inlineStyle={inlineStyle}
+  onLoad={()=> this.setState({ loaded: true })}
+  onError={() => console.error('Error')}
+/>
+```
+
+<p align="center">
+  <img src="https://3axap4eHko.github.io/react-native-katex/screenshot.png" width="380" alt="React Native KaTeX">
+</p>
+
+## License
+License [The MIT License](http://opensource.org/licenses/MIT)
+Copyright (c) 2017 Ivan Zakharchanka
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { WebView } from 'react-native';
+
+import katexStyle from './katex-style';
+import katexScript from './katex-script';
+import { bool, func, object, string } from 'prop-types';
+
+function getContent({ inlineStyle, expression = '', ...options }) {
+  return `<!DOCTYPE html>
+<html>
+<head>
+<style>
+${katexStyle}
+${inlineStyle}
+</style>
+<script>
+window.onerror = e => document.write(e);
+window.onload = () => katex.render(${JSON.stringify(expression)}, document.body, ${JSON.stringify(options)});
+${katexScript}
+</script>
+</head>
+<body>
+</body>
+</html>
+`;
+}
+
+export default class Katex extends Component {
+
+  static propTypes = {
+    expression: string.isRequired,
+    displayMode: bool,
+    throwOnError: bool,
+    errorColor: string,
+    inlineStyle: string,
+    macros: object,
+    colorIsTextColor: bool,
+    onLoad: func,
+    onError: func,
+  };
+
+  static defaultProps = {
+    displayMode: false,
+    throwOnError: false,
+    errorColor: '#f00',
+    inlineStyle: '',
+    macros: {},
+    colorIsTextColor: false,
+    onLoad: () => {},
+    onError: () => {},
+  };
+
+  render() {
+    const { style, onLoad, onError, ...options } = this.props;
+
+    return <WebView
+      style={style}
+      source={{ html: getContent(options) }}
+      onLoad={onLoad}
+      onError={onError}
+      renderError={onError}
+    />;
+  }
+}
